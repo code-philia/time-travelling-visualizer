5503146bceb3d6842482ef28b9cf4a54d05dd66e
Yvonnefanf <m18522608197@163.com>
2024-03-04 20:46:05 +0800

    fix

--- VisTool/Backend/server/Frontend/request.js
+++ VisTool/Backend/server/Frontend/request.js
@@ -8,16 +8,17 @@ headers.append('Content-Type', 'application/json');
 headers.append('Accept', 'application/json');
 
 // updateProjection
-function updateProjection() {
-    fetch(`${window.location.href}/updateProjection`, {
+function updateProjection(content_path, iteration) {
+    console.log(content_path,iteration)
+    fetch(`${window.location.href}updateProjection`, {
         method: 'POST',
         body: JSON.stringify({
-            "path": '/home/yifan/0ExpMinist/Dropout/0.5/01', 
-            "iteration": 1,
+            "path": content_path, 
+            "iteration": iteration,
             "resolution": 200,
             "vis_method": 'Trustvis',
             'setting': 'normal',
-            "content_path": '/home/yifan/0ExpMinist/Dropout/0.5/01',
+            "content_path": content_path,
             "predicates": {}
         }),
         headers: headers,
@@ -26,10 +27,25 @@ function updateProjection() {
     .then(response => response.json())
     .then(res => {
         drawCanvas(res);
+        window.vueApp.prediction_list = res.prediction_list
+        window.vueApp.label_list = res.label_list
+        window.vueApp.label_name_dict = res.label_name_dict
+        window.vueApp.evaluation = res.evaluation
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
+}
+function fetchTimelineData(content_path){
+    fetch(`${window.location.href}/get_itertaion_structure?path=${content_path}&method=Trustvis&setting=normal`, {
+        method: 'POST',
+        headers: headers,
+        mode: 'cors'
+      })
+      .then(response => response.json())
+        .then(res => {
+            drawTimeline(res)
+        })
 }
 
 
