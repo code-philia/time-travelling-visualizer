9cccdf53be09ac794bdf082a1c7b295cb34c0524
Yvonnefanf <m18522608197@163.com>
2024-03-04 15:06:56 +0800

    fix

---
+++ VisTool/Backend/server/Frontend/request.js
@@ -0,0 +1,30 @@
+// defined headers
+let headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+
+// updateProjection
+function updateProjection() {
+    fetch(`${window.location.href}/updateProjection`, {
+        method: 'POST',
+        body: JSON.stringify({
+            "path": '/home/yifan/0ExpMinist/Dropout/0.5/01', 
+            "iteration": 1,
+            "resolution": 200,
+            "vis_method": 'Trustvis',
+            'setting': 'normal',
+            "content_path": '/home/yifan/0ExpMinist/Dropout/0.5/01',
+            "predicates": {}
+        }),
+        headers: headers,
+        mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(res => {
+        drawChart(res);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
