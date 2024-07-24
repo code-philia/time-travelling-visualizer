e694a8c30299e93f5a56257302c4526628ee6c27
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2024-07-25 03:56:57 +0800

    feat: + remove panel view + add data point info view
    refactor:
    + decouple the single global message handler for extension
    + decouple extension uri to a persistent module variable

--- extension/src/resources.ts
+++ extension/src/resources.ts
@@ -1,16 +1,22 @@
 import * as vscode from "vscode";
 
-const iconPaths: {[key: string]: string} = {
+let resourceUri: vscode.Uri = vscode.Uri.file('.');
+
+export function setResourceUri(uri: vscode.Uri) {
+  resourceUri = uri;
+}
+
+export const iconPaths: {[key: string]: string} = {
   "image-type": "resources/imagesmode_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg",
   "text-type": "resources/title_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg",
   "classification-task": "resources/category_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg",
   "non-classification-task": "resources/circle_24dp_5F6368_FILL0_wght300_GRAD0_opsz24.svg"
 };
 
-export function getIconUri(context: vscode.ExtensionContext, iconName: string): vscode.Uri {
+export function getIconUri(iconName: string): vscode.Uri {
   const relativeIconPath = iconPaths[iconName];
   if (relativeIconPath === undefined) {
     throw new Error(`Icon name ${iconName} not found`);
   }
-  return vscode.Uri.file(context.asAbsolutePath(relativeIconPath));
+  return vscode.Uri.joinPath(resourceUri, relativeIconPath);
 }
