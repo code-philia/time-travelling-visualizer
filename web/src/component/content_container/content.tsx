0202d60e4e8c0aca80b47460fa9e01b0c1102305
guanjie chen <guanjie.chen@sjtu.edu.cn>
2024-10-26 21:12:41 +0800

    remove

---
+++ web/src/component/content_container/content.tsx
@@ -0,0 +1,15 @@
+export function contentContainer() {
+
+    return (
+        <div className="content_container">
+            <div id="container_range">
+                <div id="container">
+                </div>
+            </div>
+            <div id="footer">
+                <div>Epochs</div>
+                <svg id="timeLinesvg" height="0" width="0"></svg>
+            </div>
+        </div>
+    )
+}
