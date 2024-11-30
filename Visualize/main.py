980b0b79b839bacef8cd98f94c2a85f7e516ec73
HaHaKwy <1625827540@qq.com>
2024-11-30 17:00:41 +0800

    add getting representation process

--- Visualize/main.py
+++ Visualize/main.py
@@ -1,4 +1,11 @@
+import os
+import logging
 import argparse
+from config import VisConfig
+from data_provider import DataProvider
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='app.log', filemode='w')
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Time Travelling Visualizer')
@@ -8,15 +15,28 @@ def parse_args():
                        help='Visualization method')
     return parser.parse_args()
 
-def run(args):
-    # Use args.input, args.output, args.format as needed
-    # step 1: get high dimention representation
-
-    # step 2: train visualize model
-
-    # step 3: use visualize model to get 2-D embedding
-
+# ================ pipeline funcs ================
+def generate_representation(config):
+    dataProvider = DataProvider(config)
+    dataProvider.generate_representation()
+    logging.info("Representation generation finished")
+    
+def train_visualize_model(config):
     pass
+
+def generate_embedding(config):
+    pass
+
+def run(args):
+    # step 0: initialize config
+    config = VisConfig(os.path.join(args.content_path, 'config.json'))
+    # step 1: generate high dimention representation
+    generate_representation(config)
+    # step 2: train visualize model
+    train_visualize_model(config)
+    # step 3: use visualize model to get 2-D embedding
+    generate_embedding(config)
+    
 
 if __name__ == "__main__":
     args = parse_args()
