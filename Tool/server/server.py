9bd0ed5586c3dca8a213688ffd9acc09144478c1
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2025-01-03 19:31:33 +0800

    dev: add live reload for original backend

--- Tool/server/server.py
+++ Tool/server/server.py
@@ -16,12 +16,15 @@ sys.path.append('..')
 sys.path.append('.')
 
 # flask for API server
-app = Flask(__name__,static_folder='../Frontend')
+app = Flask(__name__, static_url_path='/static', static_folder='../Frontend')
 cors = CORS(app, supports_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # TODO:from where to get config path?
 config_file ='/path/to/config.json'
+
+# Check for "--dev" argument
+is_dev_mode = "--dev" in sys.argv
 
 @dataclass
 class TempConfig:
@@ -33,7 +36,7 @@ class TempConfig:
 
 @app.route("/", methods=["GET", "POST"])
 def GUI():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory('../Frontend', 'index.html')
 
 # Func: get iteration structure
 @app.route('/get_itertaion_structure', methods=["POST", "GET"])
@@ -63,29 +66,29 @@ def update_projection():
     predicates = req['predicates']
     indicates = list(range(100)) # we now don't use req['selectedPoints'] to filter in backend
 
-    # load config from config_file    
+    # load config from config_file
     config = TempConfig(req['taskType'], req['contentPath'])
     # config = initailize_config(config_file)
-    
+
     # load visualization result of one epoch
     if config.TASK_TYPE == 'classification' or config.TASK_TYPE == 'non-classification':
 
         embedding_2d, grid, decision_view, label_name_dict, label_color_list, label_list, max_iter, training_data_index, \
         testing_data_index, eval_new, prediction_list, selected_points, error_message_projection, color_list, \
             confidence_list = update_epoch_projection(config, iteration, predicates, indicates)
-        
+
         # make response and return
         grid = np.array(grid)
         color_list = color_list.tolist()
-        return make_response(jsonify({'result': embedding_2d, 
-                                    'grid_index': grid.tolist(), 
+        return make_response(jsonify({'result': embedding_2d,
+                                    'grid_index': grid.tolist(),
                                     'grid_color': 'data:image/png;base64,' + decision_view,
                                     'label_name_dict':label_name_dict,
-                                    'label_color_list': label_color_list, 
+                                    'label_color_list': label_color_list,
                                     'label_list': label_list,
-                                    'maximum_iteration': max_iter, 
+                                    'maximum_iteration': max_iter,
                                     'training_data': training_data_index,
-                                    'testing_data': testing_data_index, 
+                                    'testing_data': testing_data_index,
                                     'evaluation': eval_new,
                                     'prediction_list': prediction_list,
                                     "selectedPoints":selected_points.tolist(),
@@ -104,7 +107,7 @@ def update_projection():
 @cross_origin()
 def sprite_image():
     index = int(request.args.get("index"))
-    
+
     # load config from config_file
     config = initialize_config(config_file)
     if config.DATA_TYPE == "image":
@@ -122,11 +125,11 @@ def sprite_image():
                 text_save_dir_path = os.path.join(config.CONTENT_PATH, "Dataset","target", "{}.txt".format(int(index/2)))
         else:
             text_save_dir_path = os.path.join(config.CONTENT_PATH, "Dataset","source", "{}.txt".format(index))
-        
+
         sprite_texts = ''
         with open(text_save_dir_path, 'r') as text_f:
             sprite_texts = text_f.read()
-        return make_response(jsonify({"texts": sprite_texts}), 200)    
+        return make_response(jsonify({"texts": sprite_texts}), 200)
     else:
         raise ValueError("Invalid data type in config")
 
@@ -134,10 +137,10 @@ def sprite_image():
 @cross_origin()
 def sprite_text():
     index = int(request.args.get("index"))
-    
+
     # load config from config_file
     config = initialize_config(config_file)
-    
+
     if config.SHOW_LABEL:
         if index % 2 == 0: # source
             text_save_dir_path = os.path.join(config.CONTENT_PATH, "Dataset","source", "{}.txt".format(int(index/2)))
@@ -145,20 +148,22 @@ def sprite_text():
             text_save_dir_path = os.path.join(config.CONTENT_PATH, "Dataset","target", "{}.txt".format(int(index/2)))
     else:
         text_save_dir_path = os.path.join(config.CONTENT_PATH, "Dataset","source", "{}.txt".format(index))
-    
+
     sprite_texts = ''
     if os.path.exists(text_save_dir_path):
         with open(text_save_dir_path, 'r') as text_f:
             sprite_texts = text_f.read()
     else:
         print("File does not exist:", text_save_dir_path)
-  
+
     response_data = {
         "texts": sprite_texts
     }
     return make_response(jsonify(response_data), 200)
 
 def check_port_inuse(port, host):
+    import socket
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
@@ -169,14 +174,27 @@ def check_port_inuse(port, host):
     finally:
         if s:
             s.close()
+
 # for contrast
 if __name__ == "__main__":
-    import socket
-    # hostname = socket.gethostname()
-    # ip_address = socket.gethostbyname(hostname)
-    ip_address = '0.0.0.0'
-    port = 5000
-    while check_port_inuse(port, ip_address):
+    host = '0.0.0.0'
+    port = 5010
+    while check_port_inuse(port, host):
         port = port + 1
 
-    app.run(host=ip_address, port=port)
+    if not is_dev_mode:
+        app.run(host=host, port=port)
+    else:
+        from livereload import Server
+        from flask_debugtoolbar import DebugToolbarExtension
+
+        app.debug = True
+        app.config['SECRET_KEY'] = 'a-random-secret-key'
+        toolbar = DebugToolbarExtension(app)
+
+        server = Server(app.wsgi_app)
+
+        server.watch('../Frontend/**/*.css')
+        server.watch('../Frontend/**/*.html')
+        server.watch('../Frontend/**/*.js')
+        server.serve(host=host, port=port)
