4e89771b1cc146e9debb330d51180bfdd5b3e893
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2025-01-04 18:32:18 +0800

    fix: using a wrong sub-project module name

--- tool/server/utils.py
+++ tool/server/utils.py
@@ -19,7 +19,7 @@ import torch
 from typing import List
 
 from config import VisConfig
-from Visualize.data_provider import DataProvider
+from visualize.data_provider import DataProvider
 
 """Utils of new data schema"""
 # Func: infer available epochs through projection files, return a list of available epochs
@@ -30,14 +30,14 @@ def epoch_structure_from_projection(content_path):
         projection_folder = os.path.join(content_path, "visualize", visMethod, "projection")
         if not os.path.exists(projection_folder):
             continue
-        
+
         files = os.listdir(projection_folder)
-        
+
         file_numbers = [int(file.rstrip('.npy')) for file in files if file.endswith('.npy')]
         file_numbers = sorted(file_numbers)
         if file_numbers != []:
             return file_numbers, ''
-        
+
     return None, "No projection files found, can't infer epoch structure"
 
 # Func: load projection and label list (minimum requirement for 'update_projection')
@@ -47,12 +47,12 @@ def load_projection(config, content_path, vis_method, epoch):
     projection_path = os.path.join(content_path, "visualize", vis_method, "projection", f"{epoch}.npy")
     projection = np.load(projection_path)
     projection_list = projection.tolist()
-    
+
     # load label list for samples in projection
     attributes = config['dataset']['attributes']
     if 'label' not in attributes:
         raise NotImplementedError("label is not in attributes")
-    
+
     file_path_pattern = attributes['label']['source']['pattern']
     file_path = os.path.join(content_path, file_path_pattern)
     label_list = read_label_file(file_path)
@@ -64,11 +64,11 @@ def load_one_sample(config, content_path, index):
     attributes = config['dataset']['attributes']
     if 'sample' not in attributes:
         raise NotImplementedError("sample is not in attributes")
-    
+
     file_path_pattern = attributes['sample']['source']['pattern']
     file_path = file_path_pattern.replace('${index}', str(index))
     file_path = os.path.join(content_path, file_path)
-    
+
     _, file_extension = os.path.splitext(file_path)
     if file_extension == '.txt':
         sample = ""
@@ -81,7 +81,7 @@ def load_one_sample(config, content_path, index):
             with open(file_path, 'r') as f:
                 sample = f.readline()
         return 'text',sample
-        
+
     elif file_extension == '.png' or file_extension == '.jpg':
         img_stream = ""
         with open(file_path, 'rb') as img_f:
@@ -97,20 +97,20 @@ def get_all_texts(config, content_path):
     attributes = config['dataset']['attributes']
     if 'sample' not in attributes:
         return None, 'sample is not in attributes'
-    
+
     sampleConfig = attributes['sample']
     if sampleConfig['dataType'] != 'text':
         return None, 'sample is not text'
-    
+
     text_list = []
     if sampleConfig['source']['type'] == 'folder':
         parent_directory = os.path.dirname(sampleConfig['source']['pattern'])
         parent_directory = os.path.join(content_path, parent_directory)
-        
+
         files_and_folders = os.listdir(parent_directory)
         numbered_files = [f for f in files_and_folders if f.endswith('.txt') and f[:-4].isdigit()]
         numbered_files.sort(key=lambda f: int(f[:-4]))
-        
+
         # TODO: if we know index of samples, we can directly read the file by index
         for file_name in numbered_files:
             file_path = os.path.join(parent_directory, file_name)
@@ -129,7 +129,7 @@ def get_all_texts(config, content_path):
 # Func: given label file path, return python list of labels
 def read_label_file(file_path):
     _, file_extension = os.path.splitext(file_path)
-    
+
     if file_extension == '.npy':
         data = np.load(file_path)
         label_list = data.tolist()
@@ -141,28 +141,28 @@ def read_label_file(file_path):
             raise ValueError(f"Unsupported data type in .pth file: {type(data)}")
     else:
         raise ValueError(f"Unsupported file extension: {file_extension}")
-    
+
     return label_list
 
 def load_single_attribute(config, content_path, epoch, attribute):
     attributes = config['dataset']['attributes']
     if attribute not in attributes:
         raise NotImplementedError("Attribute name not found in config file")
-    
+
     # TODO other types of attributes?
     # how does 'dataType' and 'sourceType' work?
-    
+
     file_path_pattern = attributes[attribute]['source']['pattern']
     file_path = os.path.join(content_path, file_path_pattern)
     file_path = file_path.replace('${epoch}', str(epoch))
-    
+
     attr_data = read_from_file(file_path)
-    
+
     return attr_data
 
 def read_from_file(file_path):
     _, file_extension = os.path.splitext(file_path)
-    
+
     if file_extension == '.npy':
         data = np.load(file_path)
         result = data.tolist()
@@ -176,7 +176,7 @@ def read_from_file(file_path):
             raise ValueError(f"Unsupported data type in .pth file: {type(data)}")
     else:
         raise ValueError(f"Unsupported file extension: {file_extension}")
-    
+
     return result
 
 # Func: get simple filtered indices
@@ -184,51 +184,51 @@ def get_filter_result(config, content_path, epoch, filters):
     index_file_path = os.path.join(content_path, 'index.json')
     if not os.path.exists(index_file_path):
         return None, 'index.json not found'
-    
+
     indice_obj = read_file_as_json(index_file_path)
-    
+
     all_indices = indice_obj['train'] + indice_obj['test']
     result = all_indices
-    
+
     for filter in filters:
         filter_type = filter['filter_type']
         label_text_list = config['dataset']['classes']
-        
+
         if filter_type == 'label':
             filter_data = filter['filter_data']
-            
+
             attributes = config['dataset']['attributes']
             file_path_pattern = attributes['label']['source']['pattern']
             file_path = os.path.join(content_path, file_path_pattern)
             if not os.path.exists(file_path):
                 return None, 'label file not found'
-            
+
             label_list = read_label_file(file_path)
-            
+
             filtered_indices = [index for index, label in zip(all_indices, label_list) if label_text_list[label] == filter_data]
             result = list(set(result) & set(filtered_indices))
-        
+
         elif filter_type == 'prediction':
             filter_data = filter['filter_data']
-            
+
             attributes = config['dataset']['attributes']
             file_path_pattern = attributes['prediction']['source']['pattern']
             file_path_pattern = file_path_pattern.replace('${epoch}', str(epoch))
             file_path = os.path.join(content_path, file_path_pattern)
             if not os.path.exists(file_path):
                 return None, 'prediction file not found'
-            
+
             prediction_list = read_label_file(file_path)
-            
+
             filtered_indices = [index for index, label in zip(all_indices, prediction_list) if label_text_list[label] == filter_data]
             result = list(set(result) & set(filtered_indices))
-        
+
         elif filter_type == 'train':
             result = list(set(result) & set(indice_obj['train']))
-        
+
         elif filter_type == 'test':
             result = list(set(result) & set(indice_obj['test']))
-    
+
     return result,''
 
 
@@ -247,7 +247,7 @@ def initialize_config(config_file, setting = 'normal'):
 def get_embedding(config, all_data, epoch):
     embedding_path = os.path.join(config.CONTENT_PATH,'Model',f'Epoch_{epoch}', "embedding_"+config.VIS_METHOD+".npy")
     if os.path.exists(embedding_path):
-        embedding_2d = np.load(embedding_path, allow_pickle=True) 
+        embedding_2d = np.load(embedding_path, allow_pickle=True)
     else:
         # TODO: get embedding using visualization model
         # embedding_2d = context.strategy.projector.batch_project(EPOCH, all_data)
@@ -320,11 +320,11 @@ def get_train_test_data(context, EPOCH):
 def get_train_test_label(config):
     training_data_number = config.TRAINING["train_num"]
     testing_data_number = config.TRAINING["test_num"]
-    
+
     if config.TASK_TYPE == "classification":
         train_labels = get_classification_label(config, True)
         test_labels = get_classification_label(config, False)
-        
+
         if train_labels is None:
             train_labels = np.zeros(training_data_number, dtype=int)
         if test_labels is None:
@@ -337,8 +337,8 @@ def get_train_test_label(config):
         for i in range(feature_num):
             train_labels[i::feature_num] = i
             test_labels[i::feature_num] = i
-    
-    labels = np.concatenate((train_labels, test_labels), axis=0).astype(int)        
+
+    labels = np.concatenate((train_labels, test_labels), axis=0).astype(int)
     return labels
 
 def get_classification_label(config, train = True):
@@ -367,22 +367,22 @@ def get_coloring(config, label_list,colorType = 'classification-color'):
         color = color.astype(int)
     else:
         raise NotImplementedError()
-    
+
     label_color_list = color[label_list].tolist()
-    color_list = color      
+    color_list = color
     return label_color_list, color_list
-    
-    # train_data =  context.train_representation_data(EPOCH) 
-    # test_data =  context.test_representation_data(EPOCH) 
+
+    # train_data =  context.train_representation_data(EPOCH)
+    # test_data =  context.test_representation_data(EPOCH)
     # labels = get_train_test_label(context, EPOCH)
     # # coloring method
     # if ColorType == "noColoring":
     #     color = context.strategy.visualizer.get_standard_classes_color() * 255
 
-    #     color = color.astype(int)      
+    #     color = color.astype(int)
     #     label_color_list = color[labels].tolist()
     #     color_list = color
-       
+
     # elif ColorType == "singleColoring":
     #     n_clusters = 20
     #     save_test_label_dir = os.path.join(context.strategy.data_provider.content_path, 'Testing_data',ColorType+ "label" + str(EPOCH)+".pth")
@@ -391,26 +391,26 @@ def get_coloring(config, label_list,colorType = 'classification-color'):
     #         labels_kmeans_train = torch.load(save_train_label_dir)
     #         labels_kmeans_test = torch.load(save_test_label_dir)
     #     else:
-       
+
     #         kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(train_data)
 
     #         labels_kmeans_train = kmeans.labels_
     #         labels_kmeans_test = kmeans.predict(test_data)
-          
+
     #         torch.save(torch.tensor(labels_kmeans_train), save_train_label_dir)
     #         torch.save(torch.tensor(labels_kmeans_test), save_test_label_dir)
 
     #     colormap = plt.cm.get_cmap('tab10', n_clusters)
-    
-    #     colors_rgb = (colormap(np.arange(n_clusters))[:, :3] * 255).astype(int)  
+
+    #     colors_rgb = (colormap(np.arange(n_clusters))[:, :3] * 255).astype(int)
     #     label_color_list_train = [colors_rgb[label].tolist() for label in labels_kmeans_train]
     #     label_color_list_test = [colors_rgb[label].tolist() for label in labels_kmeans_test]
-        
+
     #     label_color_list = np.concatenate((label_color_list_train, label_color_list_test), axis=0).tolist()
     #     color_list = colors_rgb
 
     # else:
-    #     return         
+    #     return
 
     # return label_color_list, color_list
 
@@ -430,7 +430,7 @@ def get_standard_classes_color(class_num):
     # color = self.cmap(mesh_classes / mesh_max_class)
     # color = color[:, 0:3]
     # color = np.concatenate((color, np.zeros((1,3))), axis=0)
-    return color   
+    return color
 
 def update_epoch_projection(config, epoch, predicates, indicates=[]):
     if config.TASK_TYPE == "classification":
@@ -440,18 +440,18 @@ def update_epoch_projection(config, epoch, predicates, indicates=[]):
 
 def update_epoch_projection_classification(config, epoch, predicates, indicates):
     error_message = ""
-    
+
     # load data and labels
     all_labels = get_train_test_label(config)
     # error_message = check_labels_match_alldata(all_labels, all_data, error_message)
-    
+
     # load or create embedding_2d
     embedding_2d = get_embedding(config, None, epoch)
     if len(indicates):
         indicates = [i for i in indicates if i < len(embedding_2d)]
         embedding_2d = embedding_2d[indicates]
     # error_message = check_embedding_match_alldata(embedding_2d, all_data, error_message)
-    
+
     training_data_number = config.TRAINING["train_num"]
     testing_data_number = config.TRAINING["test_num"]
     training_data_index = list(range(training_data_number))
@@ -461,10 +461,10 @@ def update_epoch_projection_classification(config, epoch, predicates, indicates)
     # load or create background figure
     grid, b_fig = get_grid_bfig(config, epoch)
 
-    # load visualization evaluation result    
+    # load visualization evaluation result
     eval_new = get_eval_new(config, epoch)
-    
-    # load color list  
+
+    # load color list
     label_color_list, color_list = get_coloring(config, all_labels)
     if len(indicates):
         label_color_list = [label_color_list[i] for i in indicates]
@@ -473,7 +473,7 @@ def update_epoch_projection_classification(config, epoch, predicates, indicates)
     CLASSES = np.array(config.CLASSES)
     label_list = all_labels.tolist() # [2, 1, 9, 2...]
     label_name_dict = dict(enumerate(CLASSES))
-    
+
     prediction_list = []
     confidence_list = []
 
@@ -496,9 +496,9 @@ def update_epoch_projection_classification(config, epoch, predicates, indicates)
         #     top_three_indices = np.argsort(prediction_origin[i])[-3:][::-1]
         #     conf_list = [(label_name_dict[top_three_indices[j]], round(float(prediction_origin[i][top_three_indices[j]]), 1)) for j in range(len(top_three_indices))]
         #     confidence_list.append(conf_list)
-    
+
     max_iter = (config.EPOCH_END - config.EPOCH_START) // config.EPOCH_PERIOD + 1
-    
+
     # selected_points = get_selected_points(context, predicates, EPOCH, training_data_number, testing_data_number)
     selected_points = np.array(indicates)
     return embedding_2d.tolist(), grid, b_fig, label_name_dict, label_color_list, label_list, max_iter, training_data_index, testing_data_index, eval_new, prediction_list, selected_points,error_message, color_list, confidence_list
@@ -509,25 +509,25 @@ def update_epoch_projection_non_classification(config, epoch, predicates, indica
 
     error_message = ""
     t0 = time.time()
-    
+
     # load data and labels
     all_data = None
     all_labels = get_train_test_label(config)
 
     # error_message = check_labels_match_alldata(all_labels, all_data, error_message)
-    
+
     # load or create embedding_2d
     embedding_2d = get_embedding(config, all_data, epoch)
     if len(indicates):
         indicates = [i for i in indicates if i < len(embedding_2d)]
         embedding_2d = embedding_2d[indicates]
     # error_message = check_embedding_match_alldata(embedding_2d, all_data, error_message)
-    
+
     training_data_index = list(range(training_data_number))
     testing_data_index = list(range(training_data_number, training_data_number + testing_data_number))
     # error_message = check_config_match_embedding(all_data, training_data_number, testing_data_number, embedding_2d, error_message)
 
-    # load label list, precidtion list, confidance    
+    # load label list, precidtion list, confidance
     label_list = all_labels.tolist() #[0,1,0,1,...]
     if len(indicates):
         label_list = [label_list[i] for i in indicates]
@@ -535,20 +535,20 @@ def update_epoch_projection_non_classification(config, epoch, predicates, indica
     # load or create background figure
     grid, b_fig = get_grid_bfig(config, epoch)
 
-    # load visualization evaluation result    
+    # load visualization evaluation result
     eval_new = get_eval_new(config, epoch)
-    
-    # load color list  
+
+    # load color list
     label_color_list, color_list = get_coloring(config, label_list)
     label_name_dict = dict(enumerate(config.CLASSES)) #{0:"code", 1:"doc"}
-    
+
     prediction_list = []
     confidence_list = []
     for i in range(len(indicates)):
         prediction_list.append(0)
-    
+
     max_iter = (config.EPOCH_END - config.EPOCH_START) // config.EPOCH_PERIOD + 1
-    
+
     # selected_points = get_selected_points(context, predicates, EPOCH, training_data_number, testing_data_number)
     selected_points = np.array(indicates)
     return embedding_2d.tolist(), grid, b_fig, label_name_dict, label_color_list, label_list, max_iter, training_data_index, testing_data_index, eval_new, prediction_list, selected_points,error_message, color_list, confidence_list
@@ -581,7 +581,7 @@ def get_umap_neighborhood_epoch_projection(content_path: str, epoch: int, predic
     all_idx_num = len(read_file_as_json(all_indices_file))
     cmt_idx_num = len(read_file_as_json(comment_indices_file))
     code_idx_num = all_idx_num - cmt_idx_num
-    
+
     label_text_list = ['comment', 'code']
     labels = [0] * cmt_idx_num + [1] * code_idx_num
 
@@ -604,18 +604,18 @@ def get_umap_neighborhood_epoch_projection(content_path: str, epoch: int, predic
     # Read and return similarities, inter-type and intra-type
     inter_sim_file = os.path.join(epoch_folder, 'inter_similarity.npy')
     intra_sim_file = os.path.join(epoch_folder, 'intra_similarity.npy')
-    
+
     inter_sim_top_k = np.load(inter_sim_file).tolist()
     intra_sim_top_k = np.load(intra_sim_file).tolist()
-    
+
     # Read and return attention
     attention_folder = os.path.join(data_folder,'aa_possim') # gcb_tokens_temp/Model/aa_possim
     code_attention_file = os.path.join(attention_folder,'train_code_attention_aa.npy')
     nl_attention_file = os.path.join(attention_folder,'train_nl_attention_aa.npy')
-    
+
     code_attention = np.load(code_attention_file).tolist()
     nl_attention = np.load(nl_attention_file).tolist()
-    
+
     # Read the bounding box (TODO necessary?)
     bounding_file = os.path.join(epoch_folder, 'scale.npy')
     bounding_np_array = np.load(bounding_file)
