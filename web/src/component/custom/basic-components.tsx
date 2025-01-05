71ae665adf47d326e037569634bd58e1719644b7
Yuhuan Huang <hyh0u0@sjtu.edu.cn>
2025-01-05 23:53:05 +0800

    + use global custom blocks + disable search history temporarily

---
+++ web/src/component/custom/basic-components.tsx
@@ -0,0 +1,18 @@
+// TODO put these blocks to a universal file
+// TODO add resize/drag/dock-to mouse interaction
+export function FunctionalBlock(props: { label?: string; children?: null | React.ReactNode | React.ReactNode[]; }) {
+    return (
+        <div className="functional-block">
+            {props.label && <div className="functional-block-title">{props.label}</div>}
+            {props.children}
+        </div>
+    );
+}
+export function ComponentBlock(props: { label?: string; children?: null | React.ReactNode | React.ReactNode[]; }) {
+    return (
+        <div className="component-block">
+            {props.label && <div className="label">{props.label}</div>}
+            {props.children}
+        </div>
+    );
+}
