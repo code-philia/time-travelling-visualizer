b449ebf11ec169946bfec88d33ac979bf375dcbe
HaHaKwy <1625827540@qq.com>
2025-02-26 15:55:53 +0800

    minor

--- web/src/component/utils.tsx
+++ web/src/component/utils.tsx
@@ -1,4 +1,6 @@
-import { Edge } from "./types";
+import { BriefProjectionResult } from "../communication/api";
+import { Edge } from "./canvas/types";
+import { DistancePair } from "./canvas/types";
 
 export function softmax(arr: number[]): number[] {
     const expValues = arr.map(val => Math.exp(val));
@@ -73,4 +75,55 @@ export function createEdges(
     });
 
     return edges;
+}
+
+
+export function calculateSignificantPairs(allEpochsProjectionData: Record<number, BriefProjectionResult>, availableEpochs: number[], threshold = 0.3) {
+    if (availableEpochs.length < 2 || Object.keys(allEpochsProjectionData).length < availableEpochs.length) {
+        return [];
+    }
+
+    const firstEpoch = allEpochsProjectionData[availableEpochs[0]];
+    const lastEpoch = allEpochsProjectionData[availableEpochs[availableEpochs.length - 1]];
+    const pairs: DistancePair[] = [];
+
+    for (let i = 0; i < firstEpoch.labels.length; i++) {
+        if (firstEpoch.labels[i] === 1) continue;
+
+        for (let j = 0; j < firstEpoch.labels.length; j++) {
+            if (i === j) continue;
+            if (firstEpoch.labels[i] === firstEpoch.labels[j]) continue;
+
+            const startDist = euclideanDistance(firstEpoch.proj[i], firstEpoch.proj[j]);
+            const endDist = euclideanDistance(lastEpoch.proj[i], lastEpoch.proj[j]);
+            const delta = endDist - startDist;
+
+            if (Math.abs(delta) > threshold * startDist) {
+                pairs.push({
+                    indexA: i,
+                    indexB: j,
+                    startDistance: startDist,
+                    endDistance: endDist,
+                    distanceDelta: delta,
+                    labelA: firstEpoch.labels[i],
+                    labelB: firstEpoch.labels[j]
+                });
+            }
+        }
+    }
+
+    return pairs.sort((a, b) => Math.abs(b.distanceDelta) - Math.abs(a.distanceDelta));
+};
+
+const euclideanDistance = (a: number[], b: number[]) => {
+    return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
+};
+
+
+export function transferArray2Color(colorArray: [number, number, number] | undefined): string {
+    if (!colorArray) {
+        return 'rgb(116, 116, 116)';
+    }
+    const [r, g, b] = colorArray;
+    return `rgb(${r}, ${g}, ${b})`;
 }
